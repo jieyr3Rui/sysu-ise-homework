ad8cf26fb1040531b7ba952225a562769f91d7a4
jieyr3Rui <38846961+jieyr3Rui@users.noreply.github.com>
2020-06-22 13:27:17 +0800

    Update search_teacher.php

--- homework/php/search_teacher.php
+++ homework/php/search_teacher.php
@@ -1,5 +1,20 @@
 <?php
+header("Content-type:text/html;charset=UTF-8");
+$servername = "106.55.171.93";
+$username = 'root';
+$password = 'qwer1234,.';
+$dbname = 'db1';
+$port = "3306";
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+// Check connection
+if ($conn->connect_error) {
+    die("failure connection: " . $conn->connect_error);
+}
 
+$teacher_id = '10007';//_POST['teacher_id'];
 
+$sql = "SELECT project_name FROM project WHERE course_num IN 
+       (SELECT course_id FROM teach_list WHERE teacher_id = '{$teacher_id}')";
+echo $sql;
 
 ?>
