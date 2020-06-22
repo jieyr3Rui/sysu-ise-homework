bdc6a40ed19bba6a2cddbbce829363dca737c89d
jieyr3Rui <38846961+jieyr3Rui@users.noreply.github.com>
2020-06-22 13:39:21 +0800

    Update search_teacher.php

--- homework/php/search_teacher.php
+++ homework/php/search_teacher.php
@@ -14,7 +14,15 @@ if ($conn->connect_error) {
 $teacher_id = '10007';//_POST['teacher_id'];
 
 $sql = "SELECT project_name FROM project WHERE course_num IN 
-       (SELECT course_id FROM teach_list WHERE teacher_id = '{$teacher_id}')";
-echo $sql;
+       (SELECT course_id FROM teach_list WHERE teacher_id = '{$teacher_id}');";
+echo $sql . '<br>';
+$result = $conn->query($sql);
+if($result){
+    echo 'i found ' . strval($result->num_rows) . ' projects:' . '<br>';
+    while($row = $result->fetch_assoc()){
+        echo $row['project_name'] . '<br>';
+        
+    }
+}
 
 ?>
