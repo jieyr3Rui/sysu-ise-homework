c4127ddd7147aa8f195a46d69a11f42d0c4bb551
jieyr3Rui <38846961+jieyr3Rui@users.noreply.github.com>
2020-06-24 15:11:51 +0800

    Update download-all.php

--- homework/php/download-all.php
+++ homework/php/download-all.php
@@ -1,4 +1,5 @@
 <?php
+header("Content-type:text/html;charset=UTF-8");
   // Create zip
 function createZip(){
     $zip = new ZipArchive();
@@ -6,44 +7,40 @@ function createZip(){
     if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {
       exit("cannot open <$filename>\n");
     }
-    $project_num = '000007';//$_POST['project_num'];
-    $dir = "/web-file/project/{$project_num}/homework/";
-    //文件夹操作
-    if (is_dir($dir)){
-  
-      if ($dh = opendir($dir)){
-         while (($file = readdir($dh)) !== false){
-   
-           // If file
-           if (is_file($dir.$file)) {
-              if($file != '' && $file != '.' && $file != '..'){
-   
-                 $zip->addFile($dir.$file);//添加文件到zip
-              }
-           }else{
-              // If directory
-              if(is_dir($dir.$file) ){
-  
-                if($file != '' && $file != '.' && $file != '..'){
-  
-                  // Add empty directory
-                  $zip->addEmptyDir($dir.$file);//添加空文件夹到zip
-  
-                  $folder = $dir.$file.'/';
-   
-                  // Read data of the folder
-                  createZip($zip,$folder);
-                }
-              }
-           }
-         }
-         closedir($dh);
-       }
+
+    $servername = "106.55.171.93";
+    $username = 'root';
+    $password = 'qwer1234,.';
+    $dbname = 'db1';
+    $port = "3306";
+    $project_num = '000007';
+    $conn = mysqli_connect($servename, $username, $password, $dbname, $port);
+    //检查连接是否成功
+    if(!$conn){ 
+	die(“失败连接：”. mysqli_connect_error());
     }
+    $sql = "SELECT id, file FROM homework WHERE project_num='{$project_num}';";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+	//$extension = end(explode(".", $row['file']));  
+        $zip->addFile($row["file"], basename($row["file"]));
+        echo "file: " . $row["file"]. "<br>";
+      }
+    } else {
+      echo "0 results";
+    }
+    
     $zip->close();
-  }
-// Download Created Zip file
-if(isset($_POST['download'])){
+    
+    $sql = "UPDATE homework SET download_flag=1 WHERE project_num='{$project_num}';";
+    if(!mysqli_query($conn,$sql)) 
+         echo "mysql error: " .mysqli_error($conn);
+
+}
+
 echo "1";
 createZip();
 echo "2";
@@ -59,8 +56,7 @@ if (file_exists($filename)) {
     readfile($filename);
     // delete file
     unlink($filename);
-
-  }
-else{ echo "no file";}
 }
+
+
 ?>
